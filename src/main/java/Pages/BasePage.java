bfdb736b416665e323c7ea0f08d7d0db46c42291
Pedro Becerra <pedro.becerra.contractor@openpay.mx>
2024-08-18 20:51:16 -0300

    Adding new structure to Framework to adapt to Page Object Model

---
+++ src/main/java/Pages/BasePage.java
@@ -0,0 +1,9 @@
+package Pages;
+
+public class BasePage {
+    /*
+    Contiene métodos comunes a todas las páginas, como inicializar el WebDriver, esperar elementos,
+    tomar capturas de pantalla, etc.
+    Puede incluir métodos para manejar alertas, ventanas emergentes, cookies, etc.
+     */
+}
