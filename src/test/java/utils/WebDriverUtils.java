0192993822ead7311db463056c8f36120b6bce49
Pedro Becerra <pedro.becerra.contractor@openpay.mx>
2024-08-17 21:20:41 -0300

    Adding logging framework into project. Dependencies added.

--- src/test/java/utils/WebDriverUtils.java
+++ src/test/java/utils/WebDriverUtils.java
@@ -1,5 +1,6 @@
 package utils;
 
+import common.CommonSteps;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -8,9 +9,13 @@ import java.io.*;
 import java.time.Duration;
 import java.util.Properties;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class WebDriverUtils {
 
     static WebDriver driver;
+    private static final Logger log = LogManager.getLogger(WebDriverUtils.class);
     public static String getProp(String property, String config) {
 
         File propFile = new File(System.getProperty("user.dir") + "/src/test/resources/" + config + ".properties");
@@ -22,10 +27,12 @@ public class WebDriverUtils {
             ret =  prop.getProperty(property);
             ret = ret.startsWith("/s") ? System.getProperty("base_dir") + ret : ret;
 
+            log.info("Getting property: " + property + "from file: " + config + ".properties");
+
         } catch (FileNotFoundException e) {
-            System.out.println("The property file was not found");
+            log.fatal("A fatal error occur trying to read the file", e);
         } catch (IOException e) {
-            System.out.println("Can not read property file");
+            log.error("Can not read property file. {}", e.getMessage(), e);
         }
         return ret;
     }
@@ -36,19 +43,27 @@ public class WebDriverUtils {
 
         driver.navigate().to(
                 WebDriverUtils.getProp("URL","config"));
+
+        log.info("Browser created and navigating to {}",
+                WebDriverUtils.getProp("URL","config"));
     }
 
     public void clickBrowser(){
         new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.alertIsPresent()).accept();
-        System.out.println("Popup Clicked!");
         driver.switchTo().defaultContent();
+
+        log.info("Popup clicked and accepted");
     }
 
     public WebDriver getDriver(){
         return driver;
     }
     public void TearDown() {
-        if (driver != null)
+        if (driver != null){
+            driver.close();
             driver.quit();
+
+            log.info("Driver closed and quited");
+        }
     }
 }
